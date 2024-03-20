3462f007514a8a46f5c3d368f675146a5cff84a1
Benjamin Margolius <benjaminmargolius@gmail.com>
2024-03-19 20:29:58 -0700

    feat: added badge, working on datacontainer ui

--- src/app/_components/dataContainer.tsx
+++ src/app/_components/dataContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface DataContainerProps {
   product: {
@@ -27,7 +28,24 @@ interface DataContainerProps {
 
 const DataContainer: React.FC<DataContainerProps> = ({ product }) => {
   const item = product[0];
-  return <div>{item?.id}</div>;
+  return (
+    <div className="h-[100vh] w-[100vh] drop-shadow-lg">
+      <div className="flex h-[80%] w-[30%] flex-col items-center gap-3 bg-white pt-3">
+        <Image
+          src={item?.image ?? ""}
+          alt="product image"
+          width={128}
+          height={128}
+          className=""
+        ></Image>
+        <div className="mx-4 text-xl font-bold">{item?.title}</div>
+        <div className="mx-4 text-center text-sm font-light">
+          {item?.subtitle}
+        </div>
+        <div className="w-full border-t border-gray-200"> </div>
+      </div>
+    </div>
+  );
 };
 
 export default DataContainer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,10 @@ export default async function Home() {
   const data = await api.sales.getSales();
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-200">
-      <DataContainer product={data} />
+    <main className="flex min-h-screen bg-slate-200">
+      <div className="ml-4 mt-12">
+        <DataContainer product={data} />
+      </div>
     </main>
   );
 }
